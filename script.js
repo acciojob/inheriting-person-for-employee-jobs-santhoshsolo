9020e93178505d220dca14d1be17f67b58a2230e
santhoshsolo <154009312+santhoshsolo@users.noreply.github.com>
2024-11-05 21:31:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ function Person(name, age) {
 	this.age = age;
 }
 Person.prototype.greet = function(){
-	console.log(`Hello,
-	my name is ${this.name}, I am ${this.age} years old.`);
+	console.log(`Hello,my name is ${this.name}, I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -16,7 +15,7 @@ Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
 Employee.prototype.jobGreet = function() {
-	console.log(`hello, my name us ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	
 }
 
